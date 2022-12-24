479fcc9bf0cb7e9294cf61c81e3c57966a47345e
nilson <nirso@hotmail.com>
2022-12-24 18:33:58 +0100

    script finds svg element loaded from file

--- clk02/jsupdate2.js
+++ clk02/jsupdate2.js
@@ -1,16 +1,20 @@
 function clock(){
     const now = new Date();
     const ctx = document.getElementById('obj01');
-    //console.log(ctx.attributes.type.ownerDocument.body.childNodes[1].contentDocument.childNodes[0].children[3])
-    const ctx2 = ctx.attributes.type.ownerDocument.body.childNodes[1].contentDocument.childNodes[0].children[3];
-    console.log(ctx2)
+    const ctx2 = ctx.contentDocument.getElementById('layer2');
+            
+    //if(frameID<1.0){
+    //    console.log(ctx2)
+    //}
+        
     frameID = (frameID+0.6)%360
+    //frameID++
     ang = frameID.toFixed(1)
     rot = "rotate(" + ang + " 52.916672 52.916672)"
     //console.log(rot)
-    //ctx.setAttribute("transform", rot)
+    ctx2.setAttribute("transform", rot)
 
 }
 
 var frameID=0.0;
-setInterval(clock, 1000);
+setInterval(clock, 100);
